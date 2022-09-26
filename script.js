f6cf2a0cf670b3adc3635b21ce4d48834a28652a
Naim Lasker <naimlasker@gmail.com>
2022-09-26 16:42:07 +0600

    design updated

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const modal = document.querySelector(".whoflex-modal");
-const trigger = document.querySelector(".whoflex-trigger");
-const closeButton = document.querySelector(".whoflex-close-button");
+const signInModal = document.querySelector(".whoflex-signin-modal");
+const signUpModal = document.querySelector(".whoflex-signup-modal");
+const signInTrigger = document.querySelector(".signin-trigger");
+const signUpTrigger = document.querySelector(".signup-trigger");
+const signInCloseButton = document.querySelector(".whoflex-signin-close-button");
+const signUpCloseButton = document.querySelector(".whoflex-signup-close-button");
 
-function toggleModal() {
-    modal.classList.toggle("whoflex-show-modal");
-}
 
 function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
+    console.log('event', event.target);
+    if (event.target === signInModal) {
+        toggleSignInModal();
+    } else if(event.target === signUpModal) {
+        toggleSignUpModal();
     }
 }
 
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
+function toggleSignInModal() {
+    signInModal.classList.toggle("whoflex-signin-show-modal");
+    // modal.classList.add("whoflex-show-modal");
+}
+function toggleSignUpModal() {
+    signUpModal.classList.toggle("whoflex-signup-show-modal");
+    // modal.classList.add("whoflex-show-modal");
+}
+
+
+
+
+signInTrigger.addEventListener("click", toggleSignInModal);
+signUpTrigger.addEventListener("click", toggleSignUpModal);
+signInCloseButton.addEventListener("click", toggleSignInModal);
+signUpCloseButton.addEventListener("click", toggleSignUpModal);
 window.addEventListener("click", windowOnClick);
+
+
+// window.addEventListener("load", toggleSignUpModal)
