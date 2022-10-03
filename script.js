e9a55a8820dae3beec3b87a8b3e64ee71cca1d8a
Naim Lasker <naimlasker@gmail.com>
2022-10-04 00:18:31 +0600

    issue fixed

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ signUpBackButton.addEventListener('click', (e) => showEmailSignUpEmail(e))
 
 
 function checkEmailValidation() {
-    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
       if (!filter.test(email.value)) {
         errorMessage.style.display = "block";
           email.focus;
